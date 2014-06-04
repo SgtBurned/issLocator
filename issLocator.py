6a2d0e51d480fe6459983bf2221dc954c9b4354f
SgtBurned <SgtBurned@hotmail.co.uk>
2014-06-04 22:37:00 +0100

    Update issLocator.py

--- issLocator.py
+++ issLocator.py
@@ -1,39 +1,19 @@
-#Hello!
-#This is my first python program
-#Feel free to use it however you like but I would appreciate hitting me up at alex.bartis@gmail.com 
-#when you use it in one of your cool projects
-#GPL V3 licence if it matter
-#have fun tracking the ISS
-
-
 import urllib, json, threading
 from pygeocoder import Geocoder
 
 url= "https://api.wheretheiss.at/v1/satellites/25544";
-latitude = 0;
-longitude = 0;
-address = "";
 
-def work():
-	global latitude; 
-	global longitude; 
-        global address;
-	print " ";
-	response = urllib.urlopen(url);
-        data = json.loads(response.read());
-        latitude = data['latitude'];
-        longitude = data['longitude'];        
+def Fetch():
+	response = urllib.urlopen(url)
+        data = json.loads(response.read())
+        lat = data['latitude']
+        lon = data['longitude']       
 	try:
-		address =  Geocoder.reverse_geocode(latitude, longitude);
-		print address;
+		addr =  Geocoder.reverse_geocode(lat, lon)
+		return lat, lon, addr
 	except:
-		pass;
-		print "Current location is unknown";
-	printCoordinates();	
-	threading.Timer(35, work).start();
+		return 0,0
 
-def printCoordinates():
-        print "The International Space Station's current coordinates are ";
-        print "Latitude =",latitude," ","Longitude =",longitude;
-
-work ();
+if __name__ == "__main__":
+	Lat, Lon, Addr = Fetch()
+	print "Lat: {}\nLon: {}\nAddress: {}".format(Lat, Lon, Addr)
